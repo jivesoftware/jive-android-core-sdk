992f4584b11d4e0e598a4c69e6062430e4f18e2d
Mike Cumings <mike.cumings@jivesoftware.com>
2015-06-01 08:08:52 -0700

    ANDROID-2412 Multiple copies are created when embedding the same image multiple times into Discussion/Document
    Change-Id: I3f435974a5843fec9c2eacb3597c11bef5fffccf

--- jive-android-core-sdk/src/main/java/com/jivesoftware/android/mobile/sdk/core/JiveCoreRequestFactory.java
+++ jive-android-core-sdk/src/main/java/com/jivesoftware/android/mobile/sdk/core/JiveCoreRequestFactory.java
@@ -18,7 +18,7 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
 import org.apache.http.entity.mime.HttpMultipartMode;
 import org.apache.http.entity.mime.MultipartEntity;
-import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.entity.mime.content.AbstractContentBody;
 import org.apache.http.message.BasicNameValuePair;
 
 import javax.annotation.Nonnull;
@@ -191,7 +191,7 @@ public class JiveCoreRequestFactory {
     }
 
     @Nonnull
-    public HttpPost createContent(ContentEntity contentEntity, List<FileBody> fileBodies) {
+    public HttpPost createContent(ContentEntity contentEntity, List<? extends AbstractContentBody> fileBodies) {
         URI uri = uriFactory.createContentUri();
         HttpPost post = new HttpPost(uri);
         HttpEntity entity = createHttpEntity(contentEntity, fileBodies);
@@ -200,7 +200,7 @@ public class JiveCoreRequestFactory {
     }
 
     @Nonnull
-    public HttpPost createContent(String pathAndQuery, ContentEntity contentEntity, List<FileBody> fileBodies) {
+    public HttpPost createContent(String pathAndQuery, ContentEntity contentEntity, List<? extends AbstractContentBody> fileBodies) {
         URI uri = uriFactory.createURI(pathAndQuery);
         HttpPost post = new HttpPost(uri);
         HttpEntity entity = createHttpEntity(contentEntity, fileBodies);
@@ -209,7 +209,7 @@ public class JiveCoreRequestFactory {
     }
 
     @Nonnull
-    public HttpPut updateContent(ContentEntity contentEntity, List<FileBody> fileBodies) {
+    public HttpPut updateContent(ContentEntity contentEntity, List<? extends AbstractContentBody> fileBodies) {
         URI uri = uriFactory.updateContentUri(contentEntity);
         HttpPut put = new HttpPut(uri);
         HttpEntity entity = createHttpEntity(contentEntity, fileBodies);
@@ -218,7 +218,7 @@ public class JiveCoreRequestFactory {
     }
 
     @Nonnull
-    private HttpEntity createHttpEntity(Object entity, List<FileBody> fileBodies) {
+    private HttpEntity createHttpEntity(Object entity, List<? extends AbstractContentBody> fileBodies) {
         HttpEntity httpEntity;
         if (fileBodies.isEmpty()) {
             httpEntity = JsonEntity.from(jiveJson, entity);
@@ -228,7 +228,7 @@ public class JiveCoreRequestFactory {
             multipartEntity.addPart("json", JsonBody.create(json));
 
             int fileNumber = 1;
-            for (FileBody fileBody : fileBodies) {
+            for (AbstractContentBody fileBody : fileBodies) {
                 multipartEntity.addPart("file" + fileNumber, fileBody);
                 fileNumber++;
             }
@@ -250,7 +250,7 @@ public class JiveCoreRequestFactory {
     }
 
     @Nonnull
-    public HttpPost uploadImage(FileBody imageFileBody) {
+    public HttpPost uploadImage(AbstractContentBody imageFileBody) {
         URI uri = uriFactory.uploadImageUri();
         HttpPost post = new HttpPost(uri);
 
